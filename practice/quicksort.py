bb304a20bac90509e514efef6bd91abf37ea4430
jaysen <jaysen@gmail.com>
2023-11-08 17:51:40 +0200

    quick quicksort implementation

---
+++ practice/quicksort.py
@@ -0,0 +1,14 @@
+
+# practice implementation of quicksort:
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr.pop()
+        lesser = [x for x in arr if x < pivot]
+        greater = [x for x in arr if x > pivot]
+        return quicksort(lesser) + [pivot] + quicksort(greater)
+
+
+# test it out:
+print(quicksort([1, 3, 17, 2, 5, 4, 38]))
