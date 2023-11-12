4c4fbc6b6ec68d01b98c95e7dfc68d54a1170af3
jaysen <jaysen@gmail.com>
2023-11-12 18:45:37 +0200

    another version that times out for some tests - and the reference answer

--- HackerRank/intermediate/climbing_leaderboard.py
+++ HackerRank/intermediate/climbing_leaderboard.py
@@ -1,4 +1,5 @@
-def climbing_leaderboard(ranked, player):
+# works but not all tests work in time given
+def climbing_leaderboard_v1(ranked, player):
     ranks = []
 
     for score in player:
@@ -10,6 +11,40 @@ def climbing_leaderboard(ranked, player):
         ranks.append(sorted_dict.index(score) + 1)
     return ranks
 
+# works but not all tests work in time given
+def climbing_leaderboard_v2(ranked, player):
+    ranked = sorted(list(set(ranked)), reverse=True)
+    rankings = []
+    for score in player:
+        found = False
+        if score in ranked:
+            rankings.append(ranked.index(score) + 1)
+            continue
+        for i in range(len(ranked)):
+            if score > ranked[i]:
+                rankings.append(i + 1)
+                found = True
+                break
+        if not found:
+            rankings.append(len(ranked) + 1)
+    return rankings
+
+def climbing_leaderboard(ranked, player):
+    # Remove duplicates and sort the leaderboard in descending order
+    unique_ranked = sorted(list(set(ranked)), reverse=True)
+    player_ranks = []
+
+    # The index of the last ranked player's score
+    l = len(unique_ranked) - 1
+
+    for score in player:
+        # Check from the last index of unique_ranked where the player's score fits
+        while (l >= 0) and (score >= unique_ranked[l]):
+            l -= 1
+        # Player's rank is the index + 2 (since the index is 0-based and we need to account for the next rank)
+        player_ranks.append(l + 2)
+
+    return player_ranks
 
 
 if __name__ == '__main__':
