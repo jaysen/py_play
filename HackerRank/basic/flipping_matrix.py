d00cb5e57902a834e19789d9ad4980c0678a7e8e
jaysen <jaysen@gmail.com>
2023-11-16 16:44:29 +0200

    HR basic - flipping_matrix working now .

--- HackerRank/basic/flipping_matrix.py
+++ HackerRank/basic/flipping_matrix.py
@@ -1,38 +1,50 @@
 # https://www.hackerrank.com/test/4nahpm20m33/questions/di1dm3kpigj
-import numpy as np
+from numpy import array
 
-def flipping_matrix(arr):
+def flipping_matrix(matrix):
     """
     Using only row and col reverse moves, return the maximum sum of the upper left quadrant...
     :param arr: a 2d array of int
     :return: max sum of upper left quadrant
     """
-    matrix = np.array(arr)
+
     quad_size = len(matrix)//2
+    end = len(matrix) - 1
 
-    #column loop
-    for i in range(0, len(matrix)):
-        # check if quadrant portion is bigger after reverse
-        if sum(matrix[:quad_size, i]) < sum(matrix[quad_size:, i]):
-            matrix[:, i] = matrix[::-1, i]
+    # insight: for each value in the quadrant, use the maximum of the values symmetrically opposite in other quadrants:
 
-    # row loop
-    for i in range(0, len(matrix)):
-        # check if quadrant portion is bigger after reverse
-        if sum(matrix[i, :quad_size]) < sum(matrix[i, quad_size:]):
-            matrix[i, :] = matrix[i, ::-1]
+    # loop through each element of upper left quadrant:
+    quad_sum = 0
+    for i in range(0, quad_size):
+        for j in range(0, quad_size):
+            #print()
+            #print(i, j)
 
-    print(matrix)
-    print()
-    return sum(sum(matrix[:quad_size,:quad_size]))
+            # for each [i,j] in upper left quad, get the symmetrically opposite value in the other 3 quadrants:
+            upper_left = matrix[i, j]
+            #print(f"upper_left = {upper_left}")
+
+            upper_right = matrix[i, end-j]
+            #print(f"upper_right = {upper_right}")
+
+            lower_left = matrix[end-i, j]
+            #print(f"lower_left = {lower_left}")
+
+            lower_right = matrix[end-i, end-j]
+            #print(f"lower_right = {lower_right}")
+
+            #print(max(upper_left, upper_right, lower_left, lower_right))
+            quad_sum += max(upper_left, upper_right, lower_left, lower_right)
+
+    return quad_sum
 
 
-m = [[112, 42,  83, 119],
-     [56,  125, 56, 49],
+a = [[112, 42,  83,  119],
+     [56,  125, 56,  49],
      [15,  78,  101, 43],
      [62,  98,  114, 108]]
 
-print(np.array(m))
+m = array(a)
 print()
 print(flipping_matrix(m))
 
