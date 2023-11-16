1da464b6b148e2dac188feef46862a676515f418
jaysen <jaysen@gmail.com>
2023-11-16 16:00:12 +0200

    codewars - count_patterned_nums updated

--- CodeWars/6kyu/count_patterned_nums.py
+++ CodeWars/6kyu/count_patterned_nums.py
@@ -1,3 +1,5 @@
+# CodeWars 6kyu
+# https://www.codewars.com/kata/55d8aa568dec9fb9e200004a/
 
 def count_nums(upper_limit, max_diff):
     """
@@ -8,35 +10,43 @@ def count_nums(upper_limit, max_diff):
     """
     if upper_limit < 12:
         return 0
-    if max_diff == 0:
+    if max_diff <= 0:
         return 0
 
     count = 0
-    for num in range(12, upper_limit+1):
-        # split num into digits:
-        digits = [int(x) for x in str(num)]
-
-        # check for repeated digits: (nice pattern for that)
-        if len(digits) != len(set(digits)):
-            continue
-
-        # check for increasing digits: (nice pattern for that)
-        if digits != sorted(digits):
-            continue
-
-        # check for max_diff between digits:
-        ok_diffs = True
-        for i in range(0, len(digits)-1):
-            if (digits[i+1] - digits[i]) > max_diff:
-                ok_diffs = False
-                break
-
-        if ok_diffs:
+    for i in range(12, upper_limit+1):
+        if is_valid(i, max_diff):
             count += 1
-
     return count
+
+
+def is_valid(num, max_diff):
+    """
+    Return True if num has no repeated digits and the difference between
+    each digit is no more than max_diff.
+    """
+    # convert num to an array of digits
+    digits = [int(x) for x in str(num)]
+
+    # check for repeated digits
+    if len(digits) != len(set(digits)):
+        return False
+
+    # check for increasing digits
+    if digits != sorted(digits):
+        return False
+
+    # check for max_diff
+    for i in range(0, len(digits)-1):
+        if (digits[i+1] - digits[i]) > max_diff:
+            return False
+    return True
+
 
 
 print(count_nums(0, 1))
 print(count_nums(12, 1))
 print(count_nums(30, 2))
+print(count_nums(50, 3))
+
+
